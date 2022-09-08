d7cf16c08788f5a2b706370ca3a4775c9f5065e4
ayazhankadessova <kadessovaayazhan@gmail.com>
2022-09-08 22:16:11 +0800

    day 1

---
+++ Day 1/WelcomeJava.java
@@ -0,0 +1,8 @@
+public class WelcomeJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+        /* Print output to STDOUT. Class should be named WelcomeJava. */
+    }
+}
